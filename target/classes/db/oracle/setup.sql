5363411e3e832e351cb8595ec6693f7aace80ec1
EC2 Default User <ec2-user@ip-10-2-2-73.ap-southeast-1.compute.internal>
2025-04-12 17:38:16 +0000

    clean up old files

--- target/classes/db/oracle/setup.sql
+++
@@ -1,8 +0,0 @@
--- sqlplus sys/Welcome123_@localhost/xepdb1 as sysdba
-create tablespace demotbs datafile '/opt/oracle/oradata/demotbs01.dbf' size 10M autoextend on;
-create user demo identified by Welcome123_ default tablespace demotbs;
-alter user demo quota unlimited on demotbs;
-grant create session, resource to demo;
-grant create job to demo;
-grant execute on dbms_scheduler to demo;
-grant create materialized view to demo;

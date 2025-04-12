7c4918db851387ca8dad92e73209ea0dca423e5a
EC2 Default User <ec2-user@ip-10-2-2-73.ap-southeast-1.compute.internal>
2025-04-12 17:14:52 +0000

    setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-sudo dnf install -y java-17-amazon-corretto-devel-17 maven-amazon-corretto17 maven
+sudo dnf install -y java-17-amazon-corretto-devel-17 maven-amazon-corretto17 maven zip unzip
+
+# zip -r ../JavaBobsUsedBooks.zip . -x "*.git*"
+wget https://github.com/luodonghua/JavaBobsUsedBooks/raw/refs/heads/main/JavaBobsUsedBooks.zip 
+unzip JavaBobsUsedBooks.zip
+cd JavaBobsUsedBook
 
 sqlplus sys/Welcome123_@localhost/XEPDB1 AS SYSDBA << EOD
 @src/main/resources/db/oracle/setup.sql

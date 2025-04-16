bc72ae8d36749d604cfc5b066de7cf9f0d810f6c
Luo Donghua <luodonghua@gmail.com>
2025-04-16 19:54:15 +0800

    Update setup.sql

--- src/main/resources/db/oracle/setup.sql
+++ src/main/resources/db/oracle/setup.sql
@@ -1,6 +1,6 @@
 -- sqlplus sys/Welcome123_@localhost/xepdb1 as sysdba
 create tablespace demotbs datafile '/opt/oracle/oradata/demotbs01.dbf' size 10M autoextend on;
-create user demo identified by Welcome123_ default tablespace demotbs;
+create user demo identified by "Welcome123_" default tablespace demotbs;
 alter user demo quota unlimited on demotbs;
 grant create session, resource to demo;
 grant create job to demo;
